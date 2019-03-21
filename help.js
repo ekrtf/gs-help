ed94c807874dacfc9c57150cb58062dfe7e37619
Emile Kratiroff <emile@kratiroff.com>
2019-03-21 14:49:22 +0100

    initial

---
+++ help.js
@@ -0,0 +1,37 @@
+window.onlaod = function() {
+	// what to do when we recieve the request
+	var responseHandler = function () {
+		console.log("response text", this.responseText);
+		var response = JSON.parse(this.responseText);
+		console.log("status text", this.statusText);
+		console.log("status code", this.status);
+	};
+
+	var requestFailed = function () {
+		console.log("response text", this.responseText);
+		console.log("status text", this.statusText);
+		console.log("status code", this.status);
+	};
+
+	// make a new request
+	var request = new XMLHttpRequest();
+
+	// listen for errors
+	request.addEventListener("error", requestFailed);
+
+	// listen for the request response
+	request.addEventListener("load", responseHandler);
+
+	// ready the system by calling open, and specifying the url
+	request.open("GET", "https://swapi.co/api/people/1");
+
+	// send the request
+	request.send();
+
+	var doSubmit = function (event) {
+		var input = document.querySelector('#url');
+		var url = input.value;
+	};
+
+	document.querySelector('#submit').addEventListener('click', doSubmit);
+}
