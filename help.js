02a4014bb2ad91ca0b1a95f634525e00e2e33d11
Emile Kratiroff <emile@kratiroff.com>
2019-03-21 14:58:25 +0100

    request function

--- help.js
+++ help.js
@@ -1,37 +1,40 @@
-window.onlaod = function() {
-	// what to do when we recieve the request
-	var responseHandler = function () {
-		console.log("response text", this.responseText);
-		var response = JSON.parse(this.responseText);
-		console.log("status text", this.statusText);
-		console.log("status code", this.status);
+window.onload = function () {
+	var sendRequest = function (url) {
+		// what to do when we recieve the request
+		var responseHandler = function () {
+			console.log("response text", this.responseText);
+			var response = JSON.parse(this.responseText);
+			console.log("status text", this.statusText);
+			console.log("status code", this.status);
+		};
+
+		var requestFailed = function () {
+			console.log("response text", this.responseText);
+			console.log("status text", this.statusText);
+			console.log("status code", this.status);
+		};
+
+		// make a new request
+		var request = new XMLHttpRequest();
+
+		// listen for errors
+		request.addEventListener("error", requestFailed);
+
+		// listen for the request response
+		request.addEventListener("load", responseHandler);
+
+		// ready the system by calling open, and specifying the url
+		request.open("GET", "https://swapi.co/api/people/1");
+
+		// send the request
+		request.send();
 	};
-
-	var requestFailed = function () {
-		console.log("response text", this.responseText);
-		console.log("status text", this.statusText);
-		console.log("status code", this.status);
-	};
-
-	// make a new request
-	var request = new XMLHttpRequest();
-
-	// listen for errors
-	request.addEventListener("error", requestFailed);
-
-	// listen for the request response
-	request.addEventListener("load", responseHandler);
-
-	// ready the system by calling open, and specifying the url
-	request.open("GET", "https://swapi.co/api/people/1");
-
-	// send the request
-	request.send();
 
 	var doSubmit = function (event) {
 		var input = document.querySelector('#url');
 		var url = input.value;
+		sendRequest(url);
 	};
 
 	document.querySelector('#submit').addEventListener('click', doSubmit);
-}
+};
